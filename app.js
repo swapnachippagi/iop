d2d696549162c9a7fcd2b01a9c297315d2f8b4d4
swapnachippagi <38342013+swapnachippagi@users.noreply.github.com>
2018-06-16 10:44:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,55 +49,7 @@ angular.module('app', [])
      console.log("############");
       return !!$scope.user;
     };
-
-    $scope.loadComments = function() {
-      steem.api.getContentReplies($scope.parentAuthor, $scope.parentPermlink, function(err, result) {
-        if (!err) {
-          $scope.comments = result.slice(-5);
-          $scope.$apply();
-        }
-      });
-    };
-
-    $scope.comment = function() {
-      $scope.loading = true;
-      var permlink = steem.formatter.commentPermlink($scope.parentAuthor, $scope.parentPermlink);
-      api.comment($scope.parentAuthor, $scope.parentPermlink, $scope.user.name, permlink, '', $scope.message, '', function(err, result) {
-        console.log(err, result);
-        $scope.message = '';
-        $scope.loading = false;
-        $scope.$apply();
-        $scope.loadComments();
-      });
-    };
-
-    $scope.vote = function(author, permlink, weight) {
-      api.vote($scope.user.name, author, permlink, weight, function (err, result) {
-        if (!err) {
-          alert('You successfully voted for @' + author + '/' + permlink);
-          console.log('You successfully voted for @' + author + '/' + permlink, err, result);
-          $scope.loadComments();
-        } else {
-          console.log(err);
-        }
-      });
-    };
-
-    $scope.updateUserMetadata = function(metadata) {
-      api.updateUserMetadata(metadata, function (err, result) {
-        if (!err) {
-          alert('You successfully updated user_metadata');
-          console.log('You successfully updated user_metadata', result);
-          if (!err) {
-            $scope.user = result.account;
-            $scope.metadata = JSON.stringify(result.user_metadata, null, 2);
-            $scope.$apply();
-          }
-        } else {
-          console.log(err);
-        }
-      });
-    };
+ 
 
     $scope.logout = function() {
       api.revokeToken(function (err, result) {
