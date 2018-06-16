fa71b8b7842bfc1b9c9ef26ebec837c4434852f1
swapnachippagi <38342013+swapnachippagi@users.noreply.github.com>
2018-06-16 12:12:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ angular.module('app', [])
       var permlink = steem.formatter.commentPermlink($scope.parentAuthor, $scope.parentPermlink);
   ///$scope.parentAuthor, $scope.parentPermlink,permlink
   
-      api.comment('','f', $scope.user.name,'f-1', 'I dont know ',{tags : ['ABC']}, function(err, result) {
+      api.comment('chaithanya97','f', $scope.user.name,'f-1', 'I dont know ',{tags : ['ABC']}, function(err, result) {
         console.log(err, result);
          
         
